47cdc9cae6fdb0b7e880692d0b5eb34dfb6d1e3e
tspython <tspython@hotmail.com>
2023-01-25 17:01:28 -0800

    final hw2

--- hw2/HW2P2Shrivastav.py
+++ hw2/HW2P2Shrivastav.py
@@ -6,21 +6,23 @@ def spam ():
     def inner (): # nested function
         c="funtimes!" # another c local to inner()
         def innerinner(): # nested function
-            global a # 
-            a=54
-            nonlocal c
+            global a # creating a global variable a 
+            a=54 
+            # creating two non-local variables not local to innerinner()
+            nonlocal c 
             c=54
             nonlocal bbeans
             bbeans=68
-        innerinner()
-        print(c)
-        return bbeans
-    print(x)
-    print(c)
-    return inner()
-print(spam()) # printing whatever soan returns, which is whatever inner() returns
+        innerinner() # call innerinner()
+        print(c) # this will print 54, because of the innerinner() call   
+        return bbeans # returns 68
+    print(x) # prints 10, no change to global var x
+    print(c) # prints 14 because c local to spam() is unchanged
+    return inner() #now returns bbeans which is 68 
+    # thus order of print is 10, 14, 54, 68
+print(spam()) # printing whatever spam returns, which is whatever inner() returns
 
-# output
+# output is this because of order of how python interprets the code
 # 10
 # 14
 # 54
--- hw2/HW2P3Shrivastav.py
+++ hw2/HW2P3Shrivastav.py
@@ -1,18 +1,32 @@
 def foo(x):
-    x[0]=55
-    x=11
-    return x
+    x[0]=55 # directly modifying x
+    x=11 # locally setting x as 11
+    return x # returning this local x
 def bar(xs):
-    xs=[1, 2, 3]
-    xs[0]="squeegee"
-    return xs
-ys=[[1, 2, 3], 2, 3]
-print(foo(ys))
-print(ys)
-print(bar(ys))
-print(ys)
-zs=range(0, 10)
-print(foo(zs))
-print(zs)
-print(bar(zs))
-print(zs)
+    xs=[1, 2, 3] # locally creating a new list xs
+    xs[0]="squeegee" # this modifies the local above
+    return xs # 
+ys=[[1, 2, 3], 2, 3] # creating a list
+print(foo(ys)) # printing what we set x to which is 11
+print(ys) # ys was modified by foo(ys) call earlier, making it [55, 2, 3]
+print(bar(ys)) # prints what bar() returns which is ['squeegee', 2, 3]
+print(ys) # still will be [55, 2, 3]
+zs=range(0, 10) # creating a range object zs
+print(foo(zs)) # will give us an error 
+# if we comment the error giving line then...
+print(zs) # prints range(0,10) essentially printing the range object
+print(bar(zs)) # bar() doesn't use any operating on zs
+print(zs) # still prints range(0,10) 
+
+# output 
+# 11
+# [55, 2, 3]
+# ['squeegee', 2, 3]
+# [55, 2, 3]
+# Traceback (most recent call last):
+#  File "/home/tushar/CSCI-169/hw2/HW2P3Shrivastav.py", line 15, in <module>
+#    print(foo(zs))
+#  File "/home/tushar/CSCI-169/hw2/HW2P3Shrivastav.py", line 2, in foo
+#    x[0]=55 # directly modifying x
+# TypeError: 'range' object does not support item assignment
+
